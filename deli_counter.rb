29d889d6acabcf103ea651c6c1e3f78c135f8e3c
Ingrid sura <sura.ingrid@gmail.com>
2019-11-11 21:00:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 katz_deli=[]
-def line (array)
+ def line (array)
 if array.length 
 puts  "The line is currently empty"
 else 
 response="The line is currently:"
+array.each_with_index do|name, index| 
+response += #{index+1}. #{name}
+end
+puts response 
+end
+end
+
